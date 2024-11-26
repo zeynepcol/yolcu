3833c58176c63f18059baf1f75b63739c5cbb3ce
zeynepcol <zzeynep.coll@gmail.com>
2024-11-26 13:37:17 +0300

    update campaigns_page.dart

--- lib/pages/campaigns_page.dart
+++ lib/pages/campaigns_page.dart
@@ -25,6 +25,135 @@ class CampaignTab extends StatelessWidget{
               ],
             ),
           ),
-    
+          Padding(padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildCategoryButton("Yurtiçi Kampanyalar",true),
+                _buildCategoryButton("Yurrtdışı Kampanyalar",false),
+              ],
+            ),
+          ),
+
+          Expanded(
+              child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                children: [
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.push(context,
+                        MaterialPageRoute(
+                          builder: (context) => CampaignDetailPage(),
+                        ),
+                      );
+                    },
+                    child: _buildCampaignCard(
+                      "Turkcell Platinum Black'lilere özel yolcu360 İndirim Kampanyası!",
+                      "assets/images/campaign1.jpg",
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.push(context,
+                        MaterialPageRoute(
+                          builder: (context) => CampaignDetailPage(),
+                        ),
+                      );
+                    },
+                    child: _buildCampaignCard(
+                      "Turkcell Platinum kullanıcılarına özel yolcu360 İndirim Kampanyası!",
+                      "assets/images/campaign2.jpg",
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.push(context,
+                        MaterialPageRoute(
+                          builder: (context) => CampaignDetailPage(),
+                        ),
+                      );
+                    },
+                    child: _buildCampaignCard(
+                      "Türkiye Barolar Birliği Üyeleri Yola İndirimli Çıkıyor!",
+                      "assets/images/campaign3.jpg",
+                    ),
+                  ),
+                ],
+              )
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFilterIcon(IconData icon,String label,bool isSelected){
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: isSelected ? Colors.orange : Colors.white,
+          size: 30,
+        ),
+        SizedBox(height: 4),
+        Text(
+          label,
+          style: TextStyle(
+            color: isSelected ? Colors.orange : Colors.white,
+            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCategoryButton(String label, bool isSelected){
+    return Expanded(child: Container(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: isSelected ? Colors.blue.shade800 : Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: Colors.blue.shade800),
+      ),
+      child: Center(
+        child: Text(
+          label,
+          style: TextStyle(
+            color: isSelected ? Colors.white : Colors.blue.shade800,
+            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+          ),
+        ),
+      ),
+    ),
+    );
+  }
+
+  Widget _buildCampaignCard(String title, String imagePath){
+    return Card(
+      margin: EdgeInsets.only(bottom: 16),
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+            child: Image.asset(
+              imagePath,
+              height: 180,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(padding: const EdgeInsets.all(16.0),
+            child: Text(
+              title,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

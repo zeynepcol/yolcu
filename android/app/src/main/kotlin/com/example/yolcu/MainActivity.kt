e5f005821197cb3211e449c1cfd1dca8cfff8d6c
zeynepcol <zzeynep.coll@gmail.com>
2024-11-26 13:24:20 +0300

    update main

---
+++ android/app/src/main/kotlin/com/example/yolcu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yolcu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:yolcu/pages/home_pages.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Yolcu360 Uygulaması',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+
+  }
+}

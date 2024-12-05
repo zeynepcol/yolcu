f1afcc986e2c3b33afa12e2810e929066f84b1cb
zeynepcol <zzeynep.coll@gmail.com>
2024-12-05 14:18:00 +0300

    create StatelessWidget for more_tab.dart

---
+++ lib/pages/more_tab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MoreTab extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Diğer'),
+        centerTitle: true,
+      ),
+
+    );
+  }
+}
